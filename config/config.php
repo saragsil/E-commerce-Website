1132ef468d42146546eb6e16e3136eeaecda1403
saragsil <40894848+saragsil@users.noreply.github.com>
2024-06-20 11:21:54 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -7,11 +7,11 @@ $host = "localhost";
 
 //dbname
 
-$dbname = "Motoroda";
+$dbname = "";
 
 //username
 
-$user = "root";
+$user = "";
 
 //password
 
@@ -20,7 +20,7 @@ $pass = "";
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$secret_key = "sk_test_51M94h5Hp65tXrQ3P4wJRttxoeJDyxOXhUc1p402Oc4fbmtzTK1VZ3yDF5kJKIty9Sf1ygA7WDevSht8Ba88SbSsk00GwlnUHF9";
+$secret_key = "";
 
     
 
